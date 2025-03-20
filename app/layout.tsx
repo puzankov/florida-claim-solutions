2c2b0e66c367b9f3481e7278f90b7acf718d859c
Sergey Puzankov <puzankov.sd@gmail.com>
2025-03-20 17:27:57 -0400

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
+import {GTAG} from "@/components/ui/constants";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId={GTAG}/>
       </body>
     </html>
   );
